70ca2ff8ad3efb032463a6bf9c39e115cdc32338
Xia-Fen <1776556966@qq.com>
2019-12-23 00:25:18 +0800

    xf

---
+++ src/view/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <h2 style="text-align: center;">首页</h2>
+
+    <img src="../assets/img/banner.jpg" style="width: 100%"/>
+
+    <div style="max-height: 50px">
+    <el-calendar v-model="value" ></el-calendar>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+      data() {
+        return {
+          value: new Date()
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
